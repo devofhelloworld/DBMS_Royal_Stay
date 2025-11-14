da79a39d5c6a45dacc2502f1ee712827f7398098
devofhelloworld <sujaykumarsingh6228@gmail.com>
2025-11-14 17:08:24 +0530

    Update 4.0

--- index.js
+++ index.js
@@ -58,5 +58,5 @@ app.use((req,res,next)=>{
   })
 })
 
-const PORT = process.env.DB_PORT;
+const PORT = process.env.PORT||3000;
 app.listen(PORT,()=>console.log(`Server is running on the http://localhost:${PORT}`));
