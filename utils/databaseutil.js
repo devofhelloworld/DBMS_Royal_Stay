76952d76a78967c1ebe76f246aca9128927a4c0f
devofhelloworld <sujaykumarsingh6228@gmail.com>
2025-11-14 10:05:28 +0530

    Update 3.0

--- utils/databaseutil.js
+++ utils/databaseutil.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
   
 });
 
